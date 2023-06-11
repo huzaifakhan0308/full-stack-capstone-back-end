a14d9736a4d65806039eac904e9399210c30ac5a
ohyajk <ohyajk@gmail.com>
2023-06-11 19:55:54 +0530

    Add Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:index, :show, :create, :update, :destroy] do
+    resources :rooms, only: [:index, :show, :create, :destroy]
+    resources :reservations, only: [:index, :create, :destroy]
+  end
 end
