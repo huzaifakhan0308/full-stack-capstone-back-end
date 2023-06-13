677a26799f08e6e8721ac0ece5fd8149f9a67bd3
Fene-87 <fenekayas87@gmail.com>
2023-06-13 11:16:13 +0300

    Run db migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_10_230056) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_133006) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_230056) do
     t.string "room_service"
     t.integer "beds"
     t.string "image_url"
+    t.string "reserved"
+    t.string "boolean"
     t.bigint "users_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -43,10 +45,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_230056) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_digest"
-    t.integer "rooms_count", default: 0
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "reservations", "rooms", column: "rooms_id"
