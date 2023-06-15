76037aa828766493494f2adfc2e936f203d55eb2
ohyajk <ohyajk@gmail.com>
2023-06-15 08:39:22 +0530

    Add Cors Exception

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,9 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'  # Set the appropriate origins or specify a specific origin
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+    end
+  end
