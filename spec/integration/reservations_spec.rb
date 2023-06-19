7ee023608836d016f339f083fcb923ebed2f0111
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-06-20 03:30:32 +0500

    finish API-documentation with swagger

---
+++ spec/integration/reservations_spec.rb
@@ -0,0 +1,152 @@
+require 'swagger_helper'
+
+RSpec.describe 'Reservations API', type: :request do
+  path '/users/{user_id}/reservations' do
+    get 'Retrieves all reservations for a user' do
+      tags 'Reservations'
+      produces 'application/json'
+      parameter name: :user_id, in: :path, type: :integer
+
+      response '200', 'reservations found' do
+        schema type: :array,
+               items: {
+                 type: :object,
+                 properties: {
+                   id: { type: :integer },
+                   from_date: { type: :string, format: 'date' },
+                   to_date: { type: :string, format: 'date' },
+                   users_id: { type: :integer },
+                   rooms_id: { type: :integer }
+                 },
+                 required: %w[id from_date to_date users_id rooms_id]
+               }
+
+        let(:user) { User.create(username: 'testuser', password: 'password') }
+        let!(:reservation1) do
+          Reservation.create(from_date: '2023-01-01', to_date: '2023-01-05', users_id: user.id, rooms_id: 1)
+        end
+        let!(:reservation2) do
+          Reservation.create(from_date: '2023-02-01', to_date: '2023-02-05', users_id: user.id, rooms_id: 2)
+        end
+        let(:user_id) { user.id }
+
+        run_test!
+      end
+
+      response '404', 'user not found' do
+        let(:user_id) { 'invalid_id' }
+        run_test!
+      end
+    end
+
+    post 'Creates a reservation' do
+      tags 'Reservations'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :user_id, in: :path, type: :integer
+      parameter name: :reservation, in: :body, schema: {
+        type: :object,
+        properties: {
+          from_date: { type: :string, format: 'date' },
+          to_date: { type: :string, format: 'date' },
+          users_id: { type: :integer },
+          rooms_id: { type: :integer }
+        },
+        required: %w[from_date to_date users_id rooms_id]
+      }
+      parameter name: :password, in: :query, type: :string
+
+      response '201', 'reservation created' do
+        let(:user) { User.create(username: 'testuser', password: 'password') }
+        let(:user_id) { user.id }
+        let(:reservation) { { from_date: '2023-01-01', to_date: '2023-01-05', users_id: user.id, rooms_id: 1 } }
+        let(:password) { 'password' }
+
+        before do
+          allow(User).to receive(:find).and_return(double('user', authenticate: true, login: true))
+          allow(Reservation).to receive(:exists?).and_return(false)
+        end
+
+        run_test!
+      end
+
+      response '422', 'already reserved' do
+        let(:user) { User.create(username: 'testuser', password: 'password') }
+        let(:user_id) { user.id }
+        let(:reservation) { { from_date: '2023-01-01', to_date: '2023-01-05', users_id: user.id, rooms_id: 1 } }
+        let(:password) { 'password' }
+
+        before do
+          allow(User).to receive(:find).and_return(double('user', authenticate: true, login: true))
+          allow(Reservation).to receive(:exists?).and_return(true)
+        end
+
+        run_test!
+      end
+
+      response '401', 'incorrect password or user not logged in' do
+        let(:user) { User.create(username: 'testuser', password: 'password') }
+        let(:user_id) { user.id }
+        let(:reservation) { { from_date: '2023-01-01', to_date: '2023-01-05', users_id: user.id, rooms_id: 1 } }
+        let(:password) { 'invalid_password' }
+
+        before do
+          allow(User).to receive(:find).and_return(double('user', authenticate: false, login: false))
+          allow(Reservation).to receive(:exists?).and_return(false)
+        end
+
+        run_test!
+      end
+    end
+  end
+
+  path '/reservations/{id}' do
+    delete 'Deletes a reservation' do
+      tags 'Reservations'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :integer
+      parameter name: :password, in: :query, type: :string
+
+      response '200', 'reservation deleted' do
+        let(:reservation) do
+          Reservation.create(from_date: '2023-01-01', to_date: '2023-01-05', users_id: 1, rooms_id: 1)
+        end
+        let(:id) { reservation.id }
+        let(:password) { 'password' }
+
+        before do
+          allow(User).to receive(:find).and_return(double('user', authenticate: true, login: true))
+          allow(Room).to receive(:where).and_return(double('room', update: true))
+        end
+
+        run_test!
+      end
+
+      response '401', 'incorrect password or user not logged in' do
+        let(:reservation) do
+          Reservation.create(from_date: '2023-01-01', to_date: '2023-01-05', users_id: 1, rooms_id: 1)
+        end
+        let(:id) { reservation.id }
+        let(:password) { 'invalid_password' }
+
+        before do
+          allow(User).to receive(:find).and_return(double('user', authenticate: false, login: false))
+          allow(Room).to receive(:where).and_return(double('room', update: true))
+        end
+
+        run_test!
+      end
+
+      response '404', 'reservation not found' do
+        let(:id) { 'invalid_id' }
+        let(:password) { 'password' }
+
+        before do
+          allow(User).to receive(:find).and_return(double('user', authenticate: true, login: true))
+        end
+
+        run_test!
+      end
+    end
+  end
+end
---
+++ spec/integration/users_spec.rb
@@ -0,0 +1,209 @@
+require 'swagger_helper'
+
+describe 'Users API' do
+  path '/users' do
+    post 'Creates a user' do
+      tags 'Users'
+      consumes 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          username: { type: :string },
+          password: { type: :string },
+          password_confirmation: { type: :string }
+        },
+        required: %w[username password password_confirmation]
+      }
+
+      response '201', 'user created' do
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 username: { type: :string },
+                 password_digest: { type: :string },
+                 created_at: { type: :string, format: 'date-time' },
+                 updated_at: { type: :string, format: 'date-time' }
+               },
+               required: %w[id username created_at updated_at]
+
+        let(:user) { { username: 'testuser', password: 'password', password_confirmation: 'password' } }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:user) { { username: 'testuser' } }
+        run_test!
+      end
+    end
+  end
+
+  path '/users/{id}' do
+    get 'Retrieves a user' do
+      tags 'Users'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :integer
+
+      response '200', 'user found' do
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 username: { type: :string },
+                 password_digest: { type: :string },
+                 created_at: { type: :string, format: 'date-time' },
+                 updated_at: { type: :string, format: 'date-time' }
+               },
+               required: %w[id username created_at updated_at]
+
+        let(:id) { User.create(username: 'testuser', password: 'password', password_confirmation: 'password').id }
+        run_test!
+      end
+
+      response '404', 'user not found' do
+        let(:id) { 'invalid_id' }
+        run_test!
+      end
+    end
+  end
+
+  path '/users/{id}' do
+    put 'Updates a user' do
+      tags 'Users'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :integer
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          current_password: { type: :string },
+          username: { type: :string },
+          password: { type: :string }
+        },
+        required: %w[current_password username password]
+      }
+
+      response '200', 'user updated' do
+        schema type: :object,
+               properties: {
+                 message: { type: :string },
+                 user_id: { type: :integer },
+                 username: { type: :string },
+                 password: { type: :string }
+               },
+               required: %w[message user_id username password]
+
+        let(:user) { User.create(username: 'testuser', password: 'password', password_confirmation: 'password') }
+        let(:id) { user.id }
+        let(:current_password) { 'password' }
+        let(:username) { 'updated_username' }
+        let(:password) { 'new_password' }
+
+        before do |example|
+          example.metadata[:request] = { user: { current_password: 'password' } }
+        end
+
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:id) { 'invalid_id' }
+        let(:user) { { current_password: 'password' } }
+        run_test!
+      end
+
+      response '401', 'invalid current password' do
+        let(:user) { { current_password: 'wrongpassword' } }
+        run_test!
+      end
+    end
+  end
+
+  path '/users/{id}' do
+    delete 'Deletes a user' do
+      tags 'Users'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :integer
+
+      response '204', 'user deleted' do
+        let(:user) { User.create(username: 'testuser', password: 'password', password_confirmation: 'password') }
+        let(:id) { user.id }
+        let(:password) { 'password' }
+
+        before do |example|
+          example.metadata[:request] = { user: { username: 'testuser', password: 'password' } }
+        end
+
+        run_test!
+      end
+
+      response '401', 'invalid credentials' do
+        let(:id) { 'invalid_id' }
+        let(:password) { 'wrongpassword' }
+        run_test!
+      end
+    end
+  end
+
+  path '/users/login' do
+    post 'Logs in a user' do
+      tags 'Users'
+      consumes 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          username: { type: :string },
+          password: { type: :string }
+        },
+        required: %w[username password]
+      }
+
+      response '200', 'user logged in' do
+        schema type: :object,
+               properties: {
+                 message: { type: :string },
+                 user_id: { type: :integer },
+                 username: { type: :string },
+                 password: { type: :string }
+               },
+               required: %w[message user_id username password]
+
+        let(:user) { { username: 'testuser', password: 'password' } }
+        run_test!
+      end
+
+      response '401', 'invalid credentials' do
+        let(:user) { { username: 'testuser', password: 'wrongpassword' } }
+        run_test!
+      end
+    end
+  end
+
+  path '/users/logout' do
+    post 'Logs out a user' do
+      tags 'Users'
+      consumes 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          username: { type: :string },
+          password: { type: :string }
+        },
+        required: %w[username password]
+      }
+
+      response '200', 'user logged out' do
+        schema type: :object,
+               properties: {
+                 message: { type: :string }
+               },
+               required: ['message']
+
+        let(:user) { { username: 'testuser', password: 'password' } }
+        run_test!
+      end
+
+      response '401', 'invalid credentials' do
+        let(:user) { { username: 'testuser', password: 'wrongpassword' } }
+        run_test!
+      end
+    end
+  end
+end
